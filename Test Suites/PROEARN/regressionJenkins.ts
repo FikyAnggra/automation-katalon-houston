1bcb11d6f302bb93614badec1545d2db109f9f3d
FikyAnggra <fiky.anggra@usenobi.com>
2023-02-03 16:29:39 +0700

    update

---
+++ Test Suites/PROEARN/regressionJenkins.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>regressionJenkins</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ede008dc-032f-45e8-99ee-400aaf338cd2</testSuiteGuid>
+</TestSuiteEntity>
